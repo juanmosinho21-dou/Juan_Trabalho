64b0bd60eeb70d36b44f60ade29e6370943c8a77
juanmosinho21-dou <juanmosinho21@gmail.com>
2025-11-01 14:33:05 -0300

    Atualizando

--- Sript IF Data.R
+++ Sript IF Data.R
@@ -15,24 +15,45 @@ library(vars)
 library(ggplot2)
 library(readxl)
 library(purrr)
+library(stringr)
+library(writexl)
+library(tidyverse)
 #-------INFORMATIVENESS---------
 #Todas as series são estacionárias a um nível 5% de sig
 #Precisa coletar os dados na mão da Caixa para fechar todo o período
 #Identificar também quais os meses vão passar com erro de NA e verificar se não existe mesmo
 
 #-------CAMINHO do ZIP-----------
+
 zip_path <- "C:/Users/juanm/Downloads/arquivos renomeados.zip"
+
+IF_DATA_BACEN <- list(
+  Passivo = Passivo,
+  Ativo = Ativo,
+  DRE = DRE
+)
 
 Passivo <- IF_DATA_BACEN[grep("Passivo", names(IF_DATA_BACEN))]
 Ativo <- IF_DATA_BACEN[grep("Ativo", names(IF_DATA_BACEN))]
 DRE <- IF_DATA_BACEN[grep("Dem_Resultado", names(IF_DATA_BACEN))]
+
+Passivo_df <- as.data.frame(Passivo)
+Ativo_df   <- as.data.frame(Ativo)
+DRE_df     <- as.data.frame(DRE)
       
 #--------FILTRANDO O PATRIMONIO LIQUIDO------
+
 Passivo_df <- map_dfr(Passivo, as.data.frame)
 
 Patrimônio_Líquido_test <- Passivo_df %>%
   filter(TCB == "b1", Código %in% AT_6$Código) %>%
   select(Instituição, Código, Data, `Patrimônio.Líquido..j.`) %>%
+  rename(Patrimônio_Líquido = `Patrimônio.Líquido..j.`)
+
+
+Lucro_Líquido_test <- Lucro_Líquido1 %>%
+  filter(Código %in% AT_6$Código) %>%
+  dplyr::select(Instituição, Código, Data, `Lucro.Líquido..j.....g.....h.....i.`) %>%
   rename(Patrimônio_Líquido = `Patrimônio.Líquido..j.`)
 
 #--------FILTRANDO O LUCRO LIQUIDO----------
@@ -43,6 +64,8 @@ Lucro_Líquido <- bind_rows(DRE) %>%
 Lucro_Líquido <- Lucro_Líquido %>%         
   rename(
     Lucro_Líquido = `Lucro.Líquido..j.....g.....h.....i.`)
+
+#BUSCANDO OS QUE ESTAVAM FALTANDO
 
 #----------MEDINDO A QUANTIDADE----------
 Número_dataPL <- Patrimônio_Líquido %>%
@@ -91,26 +114,25 @@ Lucro_Líquido <- Lucro_Líquido %>%
 Patrimônio_Líquido <- Patrimônio_Líquido %>%
   mutate(Patrimônio_Líquido = as.numeric(gsub(",", ".", gsub("\\.", "", Patrimônio_Líquido))))
 
-ROE <- Lucro_Líquido %>%
+ROE_testando_todos <- Lucro_Líquido %>%
   left_join(Patrimônio_Líquido, by = c("Instituição", "Data")) %>%
   mutate(ROE = (Lucro_Líquido / Patrimônio_Líquido) * 100)
   
-ROE <- ROE %>%
-    select(-Lucro_Líquido,Patrimônio_Líquido,Código.y)
+ROE_testando_todos <- ROE_testando_todos %>%
+  dplyr::select(-Lucro_Líquido,Patrimônio_Líquido,Código.y)
 
-ROE <- ROE %>%
-  select(Data, Instituição, ROE)
+ROE_testando_todos <- ROE_testando_todos %>%
+  dplyr::select(Data, Instituição, ROE)
 
-
-Número_ROE <- ROE %>%
+Número_ROE <- ROE_testando_todos %>%
   group_by(Instituição) %>%
   summarise(n_datas = n_distinct(Data))
 
 #-----------TAXA SELIC------------
 Taxa_SELIC <- gbcbd_get_series(
-  id = 432, # Taxa Selic anualizada base 252
-  first.date = Sys.Date() - 25 * 365,
-  last.date = Sys.Date(),
+  id = 432, 
+  first.date = as.Date("2000-01-01"),
+  last.date  = as.Date("2024-12-31"),
   format.data = "long",
   be.quiet = FALSE
 )
@@ -121,10 +143,12 @@ Taxa_SELIC_Trimestral <- Taxa_SELIC %>%
   ) %>%
   group_by(Trimestre) %>%
   summarise(
-    SELIC_Média = mean(value, na.rm = TRUE),   # média do trimestre
+    SELIC_Média = mean(value, na.rm = TRUE),
     SELIC_Fim = last(value)                   
   )
 #---------TAXA SELIC TRIMESTRAL----------------
+"Ajeitar as colunas"
+
 Taxa_SELIC_Trimestral <- Taxa_SELIC_Trimestral %>%
   mutate(
     Mes = case_when(
@@ -136,30 +160,31 @@ Taxa_SELIC_Trimestral <- Taxa_SELIC_Trimestral %>%
     Ano = str_sub(Trimestre, 1, 4),
     Data = paste0(Mes, "/", Ano)
   ) %>%
-  select(Data, everything(), -Mes, -Ano, -Trimestre)
+  select(Data, SELIC_Média, SELIC_Fim)
+
 #---------ROE INDIVIDUALMENTE----------
-#PARA O SANTANDER
-ROE_BB <- ROE %>%
-  filter(Instituição %in% c("BB")) %>%
+library(dplyr)
+
+ROE_BB <- ROE_testando_todos %>%
+  filter(Instituição %in% ("BB")) %>%
   select(Data, Instituição, ROE)
 
 #PARA O SANTANDER
-ROE_SANTANDER <- ROE %>%
+ROE_SANTANDER <- ROE_testando_todos %>%
   filter(Instituição %in% c("SANTANDER BRASIL","SANTANDER BANESPA", "SANTANDER"))
 
 #PARA O BRADESCO
-ROE_BRADESCO <- ROE %>%
+ROE_BRADESCO <- ROE_testando_todos %>%
   filter(Instituição == "BRADESCO") 
 
 #PARA O CAIXA ECONOMICA FEDERAL
-ROE_CAIXA <-ROE %>%
-  filter(Instituição == "CAIXA ECONOMICA FEDERAL") 
-
+ROE_CAIXA <- ROE_testando_todos %>%
+  filter(Instituição %in% c("CAIXA ECONOMICA FEDERAL","CAIXA ECONÔMICA FEDERAL"))
 
 #PARA O ITAU
-ROE_ITAU <- ROE %>%
+ROE_ITAU <- ROE_testando_todos %>%
   filter(Instituição == "ITAU") %>%
-  select(Data, Instituição, ROE)
+  dplyr::select(Data, Instituição, ROE)
 
 #---------ADF INDIVIDUALMENTE------
 #Teste ADF para Taxa SELIC
@@ -202,7 +227,6 @@ summary(ur.df(ROE_SANTANDER$ROE, type = "drift", selectlags = "AIC"))
 #Irei fazer o var para com nível de significância de 5%
 
 #-----VETOR AUTOREGRESSIVO------
-#Não foi possível fazer porquê os estão com tamanhos diferentes, irei fazer na mão os que estão faltando
 
 VAR_todos <- data.frame(
   ROE_CAIXA = ROE_CAIXA$ROE,
@@ -213,6 +237,12 @@ VAR_todos <- data.frame(
   SELIC = Taxa_SELIC_Trimestral$SELIC_Média
 )
 
+#----BUSCANDO FALTANTES-------
 
+# EM TODOS ESTÁ FALTANDO 09/2006 E 06/2013
 
-
+FALTANTES_BB <- setdiff(Taxa_SELIC_Trimestral$Data,ROE_BB$Data)
+FALTANTES_SANTANDER <- setdiff(Taxa_SELIC_Trimestral$Data,ROE_SANTANDER$Data)
+FALTANTES_CAIXA <- setdiff(Taxa_SELIC_Trimestral$Data,ROE_CAIXA$Data)
+FALTANTES_ITAÚ <- setdiff(Taxa_SELIC_Trimestral$Data,ROE_ITAU$Data)
+FALTANTES_BRADESCO <- setdiff(Taxa_SELIC_Trimestral$Data,ROE_BRADESCO$Data)
